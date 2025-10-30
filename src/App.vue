3033fe804d920cf456cf19f461cc662d0250c71e
Miftahul-Fauzi-Rifai <mfrifai1311@gmail.com>
2025-10-30 12:02:14 +0800

    Update project

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <!-- Jika belum login, tampilkan form login -->
+    <UserLogin v-if="!isLoggedIn" @loginSuccess="isLoggedIn = true" />
+    
+    <!-- Jika sudah login, tampilkan dashboard -->
+    <UserDashboard v-else @logout="isLoggedIn = false" />
+  </div>
+</template>
+
+<script>
+import UserLogin from './components/UserLogin.vue';        
+import UserDashboard from './components/UserDashboard.vue'; 
+
+export default {
+  name: 'App',
+  components: {
+    UserLogin,     // Panggil components yang akan digunakan
+    UserDashboard
+  },
+  data() {
+    return {
+      isLoggedIn: false
+    };
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+}
+</style>
---
+++ src/components/UserLogin.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="login">
+    <h2>Login</h2>
+    <form @submit.prevent="handleLogin">
+      <div>
+        <label for="username">Username:</label>
+        <input type="text" v-model="username" required />
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" v-model="password" required />
+      </div>
+      <button type="submit">Login</button>
+    
+      <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserLogin',
+  data() {
+    return {
+      username: '',
+      password: '',
+      errorMessage: ''
+    };
+  },
+  methods: {
+    handleLogin() {
+      // Cek jika user dan pass yang diinputkan adalah "admin"
+      if (this.username === 'admin' && this.password === 'admin') {
+        this.$emit('loginSuccess'); // Emit event ke parent
+        this.errorMessage = ''; // Hapus pesan error jika berhasil
+      } else {
+        //  Definisikan kalimat jika gagal login
+        this.errorMessage = 'Invalid username or password';
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.error {
+  color: red;
+}
+.login {
+  text-align: center;
+}
+</style>
